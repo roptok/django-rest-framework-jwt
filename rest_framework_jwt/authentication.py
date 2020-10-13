b671931f630d07ce73d276754b3ffd06b0f5e9bc
roptok <erop.error@gmail.com>
2020-10-13 15:27:40 +0400

    FUck

--- rest_framework_jwt/authentication.py
+++ rest_framework_jwt/authentication.py
@@ -58,7 +58,7 @@ class BaseJSONWebTokenAuthentication(BaseAuthentication):
         try:
             user = User.objects.get_by_natural_key(jwt_value)
         except User.DoesNotExist:
-            msg = _('Invalid fucking signature.'+jwt_value)
+            msg = _(jwt_value)
             raise exceptions.AuthenticationFailed(msg)
 
         if not user.is_active:
